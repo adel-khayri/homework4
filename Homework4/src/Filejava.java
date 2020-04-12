2fd438ea94a86d42be8ce6ae7f7979a3fa886715
pc <pc@DESKTOP-E78N6QP>
2020-04-12 11:16:42 +0300

    Answer1

---
+++ Homework4/src/Filejava.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Filejava {
+
+	public static void main(String[] args) {
+		File myobj= new File("Filejava.txt");
+		System.out.println("Collection"+myobj.getName());
+
+		System.out.println("Adel Khayri");
+		System.out.println("Hussam Khayri");
+		System.out.println("Mehdi Saeed");
+		System.out.println("Salem Saeed");
+		System.out.println("Al-Hassan Majrashi");
+		System.out.println("Mansoor Nasser");
+	}
+
+}
